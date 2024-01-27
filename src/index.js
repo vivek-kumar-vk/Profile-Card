8199fd77ecef7cbcae3bd64827ec097ae1aec9d2
Vivek Kumar <vkjha2511@gmail.com>
2024-01-27 15:31:29 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="Profile\image.jpg" alt="my-imag" className="Image"></img>;
+}
+function Intro() {
+  return (
+    <div className="intro">
+      <h1>Vivek Kumar</h1>
+      <h3>
+        Hii, there I am a 🤑 Full-stack Web developer 🤑 with the hands-on
+        working knowledge on Java full stack. When i am not studying i like to
+        either watch anime or like to play online games with my friends.
+      </h3>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="Skill">
+      <Skill name="Html 👍" />
+      <Skill name="Jsp ✌️" />
+      <Skill name="Css 👍" />
+      <Skill name="Tailwind 🤗" />
+      <Skill name="JavaScript 🫰" />
+      <Skill name="React 🫰" />
+      <Skill name="Java 🫰" />
+      <Skill name="Maven 😏" />
+      <Skill name="Servlet 🔎" />
+      <Skill name="Hibernate 👍" />
+      <Skill name="Spring Boot 🤯" />
+      <Skill name="SQL ✌️" />
+      <Skill name="HQL 🫠" />
+      <Skill name="Oracle 🙋‍♂️" />
+      <Skill name="Git & GitHub 👍" />
+    </div>
+  );
+}
+function Skill(props) {
+  return <div className="skils">{props.name}</div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
